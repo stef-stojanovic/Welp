f016dc7495bfc5b5ebee4867bd90d412b640a62f
BobNarwhalKing <danceflurry4366@gmail.com>
2019-06-17 13:17:55 -0500

    Restaurant Views

--- app/models/restaurant.rb
+++ app/models/restaurant.rb
@@ -4,4 +4,17 @@ class Restaurant < ApplicationRecord
     has_many :customers, through: :orders
     has_many :customers, through: :reviews
     has_many :foods
+
+    def next_id
+        if (self.id + 1 == Restaurant.last.id + 1)
+            next_id = Restaurant.first.id
+        else next_id = self.id + 1
+        end
+    end
+    def back_id
+        if (self.id - 1 == Restaurant.first.id - 1) 
+            back_id = Restaurant.last.id
+        else back_id = self.id - 1
+        end
+    end
 end
