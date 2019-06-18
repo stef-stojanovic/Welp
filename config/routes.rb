7c7cb4d841e4dadadf4ae131a1774ed252d945d1
Stefan Stojanovic <stefan.stojanovic2202@gmail.com>
2019-06-18 16:08:02 -0500

    Customer Home Page

--- config/routes.rb
+++ config/routes.rb
@@ -3,7 +3,11 @@ Rails.application.routes.draw do
   resources :customers, :foods, :orders, :restaurants, :reviews
 
 get '/', to: 'customers#login_form'
+
 post 'customers/authenticate', to: 'customers#authenticate'
+
 get '/logout', to: 'customers#log_out'
+
+
 
 end
